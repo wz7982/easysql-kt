77b644eee1b400cd4ea511b655f3d72fc8b70abb
wangzhang <1064967982@qq.com>
2022-09-18 13:43:08 +0800

    子查询表类型推断

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -2,15 +2,23 @@ import org.easysql.database.DB
 import org.easysql.database.TableEntity
 import org.easysql.dsl.*
 import org.easysql.query.select.Select
+import org.easysql.query.select._1
+import org.easysql.query.select._2
+import org.easysql.visitor.visitExpr
 
 object Test {
     @JvmStatic
     fun main(args: Array<String>) {
-        val u1 = user alias "u1"
-        val u2 = user alias "u2"
+//        val u1 = user alias "u1"
+//        val u2 = user alias "u2"
+//
+//        val s = select(u1, u2) from u1 join u2 on (u1.id eq u2.id)
+//        println(s.sql(DB.MYSQL))
 
-        val s = select(u1, u2) from u1 join u2 on (u1.id eq u2.id)
+        val sub = select(user.id alias "col1", user.name alias "col2") from user alias "sub"
+        val s = select(sub._2(), sub._1()) from sub
         println(s.sql(DB.MYSQL))
+
 
 //        val ur = UserRow(1, null)
 //
@@ -21,6 +29,26 @@ object Test {
 //        println(u.sql(DB.MYSQL))
     }
 }
+
+//fun print(i: Int) {
+//    (1 .. i).forEach {
+//        print("@JvmName(\"_$it")
+//        (1 .. i).forEach { n ->
+//            print("T$n")
+//        }
+//        println("\")")
+//        print("fun <")
+//        (1 until i).forEach { n ->
+//            print("T$n, ")
+//        }
+//        print("T$i> SelectQuery<Tuple$i<")
+//        (1 until i).forEach { n ->
+//            print("T$n, ")
+//        }
+//        print("T$i>>._$it() = ")
+//        println("col<T$it>(\"\${this.aliasName}.\${aliasNames[${it - 1}]}\")")
+//    }
+//}
 
 class A : TableSchema<Nothing>("a") {
     val id = intColumn("id")
