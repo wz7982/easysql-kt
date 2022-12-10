ca49f6b6a247839947959d4faa6a55e2b4b4a49b
wz7982 <1064967982@qq.com>
2022-12-10 10:56:14 +0800

    开启context receiver特性

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.22"
 }
 
 group = "org.easysql"
@@ -25,4 +25,5 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
     kotlinOptions.useK2 = true
+    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
